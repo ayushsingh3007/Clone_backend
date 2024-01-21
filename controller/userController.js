0eaf75d608a4f248924da5a428f28c977a317867
ayushsingh3007 <ayushsingh6394023007@gmail.com>
2024-01-21 03:46:01 -0800

    sdkfjlksdfds

--- controller/userController.js
+++ controller/userController.js
@@ -1,7 +1,7 @@
 const bcrypt=require('bcrypt')
 const jwt=require('jsonwebtoken')
 
-const cors=require('cors');
+
 
 const { course } = require('../Schemafolders/courseSchema');
 const { str } = require('../Schemafolders/backendSchema');
