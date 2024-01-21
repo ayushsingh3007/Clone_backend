a04f546af18297faabdea7df9fd7e0dc892af457
ayushsingh3007 <ayushsingh6394023007@gmail.com>
2024-01-21 04:01:39 -0800

    bcb

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express=require('express')
 const app=express()
-const dotenv=require('dotenv')
+
 const cors=require('cors')
 const bodyparser=require('body-parser')
 const app1 = require('./routing/userRouters')
